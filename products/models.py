331423339bebbefb78c98fa8de3ccdb30eee4cf5
Angie55 <angie_stead55@yahoo.com>
2020-03-21 21:13:26 +0000

    Added category and product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, default='')
+    image = models.ImageField(upload_to='images', default='')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('products_by_category', kwargs={'pk': self.id})
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, null=True)
+    title = models.CharField(max_length=100, default='')
+    introduction = models.TextField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    milk_choc = models.BooleanField(default=True)
+    white_choc = models.BooleanField(default=False)
+    dark_choc = models.BooleanField(default=False)
+    tags = models.CharField(max_length=300)
+    image1 = models.ImageField(upload_to='images', blank=True)
+    image2 = models.ImageField(upload_to='images', blank=True, null=True)
+    image3 = models.ImageField(upload_to='images', blank=True, null=True)
+    image4 = models.ImageField(upload_to='images', blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('products_detail', kwargs={'pk': self.id})
+
+    def __str__(self):
+        return self.name

6f6b8dba6591175ed1961bf68f0a9e0d697c41bb
Angie55 <angie_stead55@yahoo.com>
2020-03-28 17:16:29 +0000

    Added cart app - created contexts, views and urls

---
+++ cart/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, reverse
+
+
+def view_cart(request):
+    """
+    The view that renders the cart page with products that have been added
+    """
+    return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    """Adds a product to the cart in specified quantity"""
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """
+    Adjusts the quantity of the product in the cart
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
--- chocify_ecommerce/urls.py
+++ chocify_ecommerce/urls.py
@@ -19,6 +19,7 @@ from home.views import index
 from accounts import urls as accounts_urls
 from products import urls as urls_products
 from products.views import all_products
+from cart import urls as urls_cart
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -28,5 +29,6 @@ urlpatterns = [
     url(r'^$', all_products),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]

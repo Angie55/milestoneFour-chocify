a406b92b705ad90078e376cd14bac460077154d0
Angie55 <angie_stead55@yahoo.com>
2020-04-07 10:49:39 +0000

    Created category page view, url and html page.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, products_by_category
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', products_by_category, name='products_by_category')
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
@@ -7,3 +7,14 @@ def all_products(request):
     categories = Category.objects.all()
     return render(request, "products.html", {"products": products,
                                              "categories": categories})
+
+
+def products_by_category(request, category_slug):
+    categories = Category.objects.all()
+    products = Product.objects.all().order_by("name")
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, "categories.html", {"products": products,
+                                               "categories": categories,
+                                               })

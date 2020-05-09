def33bebd0821f0ca63b273979af00568eab7371
Angie55 <angie_stead55@yahoo.com>
2020-05-09 19:15:54 +0000

    Added some JS so thumbnails can be clicked to become the main image

--- static/js/alert.js
+++ static/js/alert.js
@@ -5,5 +5,14 @@ window.setTimeout(function() {
         $(this).remove(); 
     });
 }, 5000);
- 
+
+$(".prod-thumb img").hover(function(){
+    $('#main-img').attr('src',$(this).attr('src').replace(/['"]?\)$/,''));
+});
+
+$('.prod-thumb').click(function() {
+        let url = $(this).attr('src').replace(/^url\(['"]?/,'').replace(/['"]?\)$/,'');
+        $('#main-img').attr('src', url);
+    });
+
 });

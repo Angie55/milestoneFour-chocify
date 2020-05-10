33ca40ab1505a1d0bd01325f7bcc2857755e2a66
Angie55 <angie_stead55@yahoo.com>
2020-05-10 12:04:36 +0000

    removed code left behind on main.js when figuring thumbnail click out. tweaks to category page

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
- 
+    
+// Code to automatically fade the alert message 
 window.setTimeout(function() {
     $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
         $(this).remove(); 
     });
 }, 5000);
 
-$(".prod-thumb img").hover(function(){
-    $('#main-img').attr('src',$(this).attr('src').replace(/['"]?\)$/,''));
-});
-
+// Code for product_details page so thumbnails can be clicked to make them the main, larger image.
 $('.prod-thumb').click(function() {
         let url = $(this).attr('src').replace(/^url\(['"]?/,'').replace(/['"]?\)$/,'');
         $('#main-img').attr('src', url);
